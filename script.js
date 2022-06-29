238d6a4c3da34c861d7b4d13f0215c28fa544298
Nilde Lima <nildenik@gmail.com>
2022-06-29 21:32:45 +0100

    event click

---
+++ script.js
@@ -0,0 +1,7 @@
+const subtrair = document.querySelector('#subtrair');
+const somar = document.querySelector('#somar');
+const braco = document.querySelector('#braco');
+
+subtrair.addEventListener('click' , () => {
+  braco.value = parseInt(braco.value) -1
+})
